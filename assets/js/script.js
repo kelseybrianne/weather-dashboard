db10f45aa104a2076b8c21fdf6f8ee2ca2b41364
Kelsey Mailau <kelseymailau@gmail.com>
2021-12-03 16:07:11 -0800

    comment js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
 var APIKey = "41200b32e10879046d4df40eb99353ec";
-console.log ($("#city")); // Working
-
 var fetchButton = $("#fetch-button");
 
 function getCityForecast(event) {
-    var city = $("#city").val(); 
     event.preventDefault();
+
+    // Get value of the city input
+    var city = $("#city").val(); 
     
     var requestCurrentUrl = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=${APIKey}`;
 
-    var today = moment().format("MM-DD-YYYY")
     
     fetch(requestCurrentUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        var lastUpdated = moment.unix(data.dt).format("h:mm A")
         console.log(data);
+        
+        // Get access to current time and date and format it for display
+        var lastUpdated = moment.unix(data.dt).format("h:mm A")
+        var today = moment().format("MM-DD-YYYY")
+
+        // Add weather icons to page
         iconURL = `http://openweathermap.org/img/wn/${data.weather[0].icon}
         @2x.png`;
+
+        // Add current weather to page for searched city
         var currentWeather = 
         `<div class="ml-3 mb-5 p-4 card bg-light text-dark text-white d-block today-custom">
             <img id="wicon" src="${iconURL}.png">
@@ -33,6 +39,7 @@ function getCityForecast(event) {
         </div>`
         $("#new-weather-data").append(currentWeather)
 
+        // Add searched city to search history list
         var currentCity = 
         `<button href="#" class="list-group-item list-group-item-action m-1 rounded" aria-current="false">${data.name}</button>`
         $("#city-history").append(currentCity)
@@ -50,6 +57,7 @@ function getCityForecast(event) {
         .then(function (data) {
             console.log(data);
 
+            // Add UV Index for current weather from data in one call API
             if (data.current.uvi <= 5) {
                 $("#uv-index").addClass("favorable");
             } else if (data.current.uvi > 5 && data.current.uvi < 8) {
@@ -59,41 +67,24 @@ function getCityForecast(event) {
             };
             $("#uv-index").text(data.current.uvi);
 
-            for(var i=0; i<5; i++) {
+            // Add five day forecast
+            for(var i=1; i<6; i++) {
                 var fiveDayForecast = 
                 `<div class="col-2 card bg-light p-0 mx-3 mb-3 card-custom" style="max-width: 18rem;">
-                    <div class="card-header day">${moment.unix(data.daily[i].dt).format("dddd MM-DD")}</div>
+                    <div class="card-header day">${moment.unix(data.daily[i].dt).format("ddd MM-DD")}</div>
                     <div class="card-body">
                         <h5 class="card-title icon"></h5>
-                        <p class="card-text">${data.daily[i].temp.day}&#176F</p>
-                        <p class="card-text">${data.daily[i].wind_speed} MPH</p>
-                        <p class="card-text">${data.daily[i].humidity}%</p>
+                        <p class="card-text">Temp: ${data.daily[i].temp.day}&#176F</p>
+                        <p class="card-text">Wind: ${data.daily[i].wind_speed} MPH</p>
+                        <p class="card-text">Humidity: ${data.daily[i].humidity}%</p>
                     </div>
                 </div>`
                 $("#five-days").append(fiveDayForecast);
-                
-
-            
-       
-
-
-
-        
-        
-
-
-                // Use the console to examine the response
-                // TODO: Loop through the data and generate your HTML
-                // for(var i=0; i<data.length; i++) {
-                //   var newHTML = 
-                //   `<h1>${data[i].login}</h1>
-                //   <p>${data[i].html_url}</p>`
-        
-                //   $("#users").append(newHTML);
-                // }
+          
             };                       
         });
     });
 };
 
+// Add search city weather function on click
 fetchButton.click(getCityForecast);
