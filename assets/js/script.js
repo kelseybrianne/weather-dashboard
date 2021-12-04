cea4f3effc5cbb6712b9d2f58f73f0d8e5446220
Kelsey Mailau <kelseymailau@gmail.com>
2021-12-04 10:56:56 -0800

    add weather icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
 var APIKey = "41200b32e10879046d4df40eb99353ec";
-var fetchButton = $("#fetch-button");
 
 function getCityForecast(event) {
     event.preventDefault();
-
+    
     // Get value of the city input
     var city = $("#city").val(); 
     
     var requestCurrentUrl = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=${APIKey}`;
-
+    
     
     fetch(requestCurrentUrl)
     .then(function (response) {
@@ -20,25 +19,26 @@ function getCityForecast(event) {
         // Get access to current time and date and format it for display
         var lastUpdated = moment.unix(data.dt).format("h:mm A")
         var today = moment().format("MM-DD-YYYY")
-
+        
         // Add weather icons to page
-        iconURL = `http://openweathermap.org/img/wn/${data.weather[0].icon}
-        @2x.png`;
-
+        iconURL = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        
         // Add current weather to page for searched city
         var currentWeather = 
         `<div class="ml-3 mb-5 p-4 card bg-light text-dark text-white d-block today-custom">
-            <img id="wicon" src="${iconURL}.png">
-            <h5 class="card-title">${data.name}</h5>
-            <h6 class="card-subtitle mb-3 text-muted">Last updated at ${lastUpdated} for ${today}</h6>
-            <p class="card-text">Temp: ${data.main.temp} &#176F</p>
-            <p class="card-text">Wind: ${data.wind.speed} MPH</p>
-            <p class="card-text">Humidity: ${data.main.humidity}%</p>
-            <p style="display: inline" class="card-text">UV Index: </p>
-            <p id="uv-index" class="rounded text-light p-2" style="display: inline"></p>
+        <img id="wicon" src="${iconURL}">
+        <h5 class="card-title">${data.name}</h5>
+        <h6 class="card-subtitle mb-3 text-muted">Last updated at ${lastUpdated} for ${today}</h6>
+        <p class="card-text">Temp: ${data.main.temp} &#176F</p>
+        <p class="card-text">Wind: ${data.wind.speed} MPH</p>
+        <p class="card-text">Humidity: ${data.main.humidity}%</p>
+        <p style="display: inline" class="card-text">UV Index: </p>
+        <p id="uv-index" class="rounded text-light p-2" style="display: inline"></p>
         </div>`
         $("#new-weather-data").append(currentWeather)
 
+        console.log(iconURL);
+        
         // Add searched city to search history list
         var currentCity = 
         `<button href="#" class="list-group-item list-group-item-action m-1 rounded" aria-current="false">${data.name}</button>`
@@ -46,9 +46,8 @@ function getCityForecast(event) {
 
         var lat = data.coord.lat
         var lon = data.coord.lon
-    
+        
         var requestFiveDayURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${APIKey}&units=imperial`;
-
         
         fetch(requestFiveDayURL)
         .then(function (response) {
@@ -73,18 +72,29 @@ function getCityForecast(event) {
                 `<div class="col-2 card bg-light p-0 mx-3 mb-3 card-custom" style="max-width: 18rem;">
                     <div class="card-header day">${moment.unix(data.daily[i].dt).format("ddd MM-DD")}</div>
                     <div class="card-body">
-                        <h5 class="card-title icon"></h5>
+                        <img id="wicon" src="http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png">
                         <p class="card-text">Temp: ${data.daily[i].temp.day}&#176F</p>
                         <p class="card-text">Wind: ${data.daily[i].wind_speed} MPH</p>
                         <p class="card-text">Humidity: ${data.daily[i].humidity}%</p>
                     </div>
                 </div>`
                 $("#five-days").append(fiveDayForecast);
-          
-            };                       
+                
+            };     
+            
+            // From the <button> CONTAINER element, listen to the <button> "click"
+                
+                // Get the city from the button's data attribute
+            
+            // currentCity.on("click", function() {
+
+            //     city = data.name
+            //     getCityForecast();
+                
+            // });
         });
     });
 };
 
 // Add search city weather function on click
-fetchButton.click(getCityForecast);
+$("#submit").submit(getCityForecast);
